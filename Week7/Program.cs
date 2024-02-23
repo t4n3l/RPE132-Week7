352100018e98239cacd248bc4e7300b10889265c
Tanel Ristsalu <tanel.ristsalu@tktk.ee>
2024-02-23 15:40:10 +0200

    Add project files.

---
+++ Week7/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+Console.WriteLine("Kas sa tuled või lähed? (tulen/lähen)");
+
+string valik = Console.ReadLine();
+
+if (valik == "tulen")
+{
+    Tervitus();
+}
+else
+{
+    Lahkumine();
+}
+
+static void Tervitus()
+{
+    Console.WriteLine("Tere, maailm!");
+}
+
+static void Lahkumine()
+{
+    Console.WriteLine("Nägemist!");
+}
