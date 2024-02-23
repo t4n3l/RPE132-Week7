f092d1e445bf7d1f2593e5f663ed17461d299b3a
Tanel Ristsalu <tanel.ristsalu@tktk.ee>
2024-02-23 16:21:27 +0200

    moodle testid

---
+++ Moodle/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Enter your comment:");
+string userInput = Console.ReadLine();
+
+CountSpaces(userInput);
+
+static void CountSpaces(string someString)
+{
+    someString = someString.Trim();
+    int wordCounter = 1;
+
+    foreach (char symbol in someString)
+    {
+        if (char.IsWhiteSpace(symbol))
+        {
+            wordCounter++;
+        }
+    }
+
+    string word = "word";
+    if (wordCounter > 1)
+    {
+        word = "words";
+    }
+
+    Console.WriteLine($"Your comment contains {wordCounter} {word}.");
+}
---
+++ Moodle2/Program.cs
@@ -0,0 +1,42 @@
+﻿
+//Kirjutage programm, mis teisendab Fahrenheiti kraadi Celsiuse järgi ja vastupidi.
+//* Programm võimaldab kasutajal valida, kas teisendada väärtuse Celciusesse või Farenheiti.
+//* Kui kasutaja on oma valiku teinud, programm küsib sisendit, teostab arvutusi ja kuvab tulemuse.
+//* fahrenheit = (celsius * 9) / 5 + 32;
+//*celsius = (fahrenheit - 32) * 5 / 9;
+
+//NB! Programmis peab olema kaks funktsiooni:
+
+//static void ConverToCelsius(argument) { }
+//static void ConvertToFahrenheit(argument) { }
+
+//Jaga programmi koodi siin (programmi koodi võib, aga ei pea, githubisse üles laadida).
+
+Console.WriteLine("Kas soovid temperatuuri teisendada Celsiuse (C) või Fahrenheiti (F) järgi?");
+char kasutajaSisend = char.ToUpper(char.Parse(Console.ReadLine()));
+Console.WriteLine("Sisesta teisendamist vajav temperatuur: ");
+int sisendTemp = Int32.Parse(Console.ReadLine());
+
+if (kasutajaSisend == 'C')
+{
+    ConvertToCelsius(sisendTemp);
+}
+else if (kasutajaSisend == 'F')
+{
+    ConvertToFahrenheit(sisendTemp);
+}
+else
+{
+    Console.WriteLine("Vale sisend!");
+}
+
+static void ConvertToCelsius(int t)
+{
+    Console.WriteLine($"{t} kraadi Farhenheiti on võrdne {(t - 32) * 5 / 9} kraadi Celsiust");
+}
+
+static void ConvertToFahrenheit(int t)
+{
+    Console.WriteLine($"{t} kraadi Celsiust on võrdne {(t * 9) / 5 + 32} kraadi Farhenheiti");
+
+}
